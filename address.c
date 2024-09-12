2845aaa778b9f470e6e6e5b8e42346a575bfa3e6
Rocky07777 <rocky420bhai011@gmail.com>
2024-09-12 15:33:28 +0530

    Add files via upload

---
+++ address.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+  int *a;
+  int b=10;
+  a=&b;
+  printf("address of b is %d\n",a);
+  printf("value of b is %d\n",*a);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
